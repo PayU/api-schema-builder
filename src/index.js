620b4dff03747c7891042f685eacb76f620a99cb
Damir <damir@yusipov.ru>
2021-03-04 13:26:42 +0300

    eslint fix

--- src/index.js
+++ src/index.js
@@ -39,9 +39,9 @@ function buildValidations(referenced, dereferenced, receivedOptions) {
     const basePaths = dereferenced.servers && dereferenced.servers.length
         ? dereferenced.servers.map(({ url, variables = {} }) => {
             Object.keys(variables).forEach((key) => {
-                url = url.replace(new RegExp(`{${key}}`), variables[key].default || '')
-            })
-            return url.slice(0,1) === '/' ? url : new URL(url).pathname
+                url = url.replace(new RegExp(`{${key}}`), variables[key].default || '');
+            });
+            return url.slice(0, 1) === '/' ? url : new URL(url).pathname;
         })
         : [dereferenced.basePath || '/'];
 
